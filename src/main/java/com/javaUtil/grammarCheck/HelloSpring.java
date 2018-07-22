3e3fb0f9d9a5fa1c2ed4ba6503e9a4cc7d4eb6f3
gia00 <gia00@DESKTOP-9UP0B48>
2018-07-22 15:45:58 +0900

    fist creat

---
+++ src/main/java/com/javaUtil/grammarCheck/HelloSpring.java
@@ -0,0 +1,22 @@
+package com.javaUtil.grammarCheck;
+
+/**
+ * <pre>
+ * com.javaUtil.grammarCheck
+ * HelloSpring.java
+ * </pre>
+ *
+ * @Description :
+ *
+ * @Author  : gia00
+ * @Date    : 2018. 7. 22.
+ * @Version :
+ */
+public class HelloSpring {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello Spring!!!");
+    }
+
+}
